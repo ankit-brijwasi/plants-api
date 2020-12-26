dab16ae033f8fb49e8829be892f2277fd9af5509
Ankit Brijwasi <abrijwasi1@gmail.com>
2020-12-26 12:04:41 +0530

    Plants model

--- nursury/models.py
+++ nursury/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from PIL import Image
 
-# Create your models here.
+User = get_user_model()
+
+class Plant(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="plant_images/")
+    price = models.DecimalField(decimal_places=3, max_digits=19)
+
+    def __str__(self):
+        return self.name
